1b56444db2d4da6dda7d1a055f2f7f048d60ee8c
IzharKarim99 <izhar@eforte.net>
2023-02-15 17:22:38 +0500

    static website built with react and tailwind

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Feedback from "./Feedback";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import CTA from "./CTA";
+
+export {Navbar,Billing,Business,Button,CardDeal,Clients,Feedback,Footer,GetStarted,Hero,Stats,Testimonials,CTA}
